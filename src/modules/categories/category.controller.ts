fddabe7e783a6d5b3a377b76836961857e58d852
Dima Golubenko <dimagolubenko@gmail.com>
2022-03-18 01:36:30 +0200

    Add category controller methods

--- src/modules/categories/category.controller.ts
+++ src/modules/categories/category.controller.ts
@@ -14,7 +14,10 @@ export class CategoryController implements ControllerInterface {
 
   private initilizeRoutes() {
     this.router.get(this.path, this.getAllCategories);
+    this.router.get(`${this.path}/:id`, this.getCategoryById);
+    this.router.put(`${this.path}/:id`, this.updateCategory);
     this.router.post(this.path, this.createCategory);
+    this.router.delete(`${this.path}/:id`, this.deleteCategory);
   }
 
   private async getAllCategories(
@@ -22,7 +25,23 @@ export class CategoryController implements ControllerInterface {
     response: express.Response
   ) {
     const categories = await categoryModel.find({});
-    response.json({ categories });
+    response.json({ data: categories });
+  }
+
+  private async getCategoryById(
+    request: express.Request,
+    response: express.Response
+  ) {
+    const categoryId = request.params.id;
+    const successResponse = await categoryModel.findById(categoryId);
+    if (!successResponse) {
+      return response.status(404).json({
+        error: true,
+        message: `Category with id ${categoryId} not found`,
+      });
+    }
+
+    return response.json({ data: successResponse });
   }
 
   private async createCategory(
@@ -31,6 +50,43 @@ export class CategoryController implements ControllerInterface {
   ) {
     const categoryData: CategoryInterface = request.body;
     const newCategory = await categoryModel.create(categoryData);
-    response.json({ data: newCategory });
+    response.status(201).json({ data: newCategory });
+  }
+
+  private async updateCategory(
+    request: express.Request,
+    response: express.Response
+  ) {
+    const categoryId = request.params.id;
+    const categoryData: CategoryInterface = request.body;
+    const updatedCategory = await categoryModel.findByIdAndUpdate(
+      categoryId,
+      categoryData,
+      { new: true }
+    );
+    if (updatedCategory) {
+      response.json({ data: updatedCategory });
+    } else {
+      response.status(404).json({
+        error: true,
+        message: `Category with id "${categoryId} not found"`,
+      });
+    }
+  }
+
+  private async deleteCategory(
+    request: express.Request,
+    response: express.Response
+  ) {
+    const categoryId = request.params.id;
+    const successResponse = await categoryModel.findByIdAndDelete(categoryId);
+    if (successResponse) {
+      response.send(204);
+    } else {
+      response.send(404).json({
+        error: true,
+        message: `Category with id "${categoryId} not found"`,
+      });
+    }
   }
 }
